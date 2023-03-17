fedefad2728aea9582fbd58ab284c780d6d9b1b1
tnemade <tnemade@uncc.edu>
2023-03-16 22:36:36 -0400

    Revert "Added streamlit import packages"
    This reverts commit f447ff7427b3c812444f6e51a789fb8fa0f351ba.

--- face_mesh.py
+++ face_mesh.py
@@ -1,8 +1,7 @@
-import streamlit as st
-import mediapipe as mp
 import cv2
-
+import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
 # For webcam input:
