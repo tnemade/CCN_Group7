3ddecad66dd9096b24b8b8c3a39944d84fa16dde
tnemade <tnemade@uncc.edu>
2023-03-16 22:48:04 -0400

    Update face_mesh.py
    Added GUI components

--- face_mesh.py
+++ face_mesh.py
@@ -5,16 +5,24 @@ import cv2
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
+st.title("Face Mesh")
+max_faces = st.number_input("Maximum Number of Faces", value = 2, min_value=1)
+st.markdown("---")
+detection_confidence = st.slider("Min Detection Confidence", min_value=0.0, max_value=1.0, value=0.5)
+tracking_confidence = st.slider("Min Tracking Confidence", min_value=0.0, max_value=1.0, value=0.5)
+st.markdown("---")
+
 stframe = st.empty()
 
 vid = cv2.VideoCapture(0)
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 with mp_face_mesh.FaceMesh(
-        max_num_faces=2,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5
+    max_num_faces = 2,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5
 ) as face_mesh:
+
     while vid.isOpened():
         ret, frame = vid.read()
         if not ret:
@@ -26,15 +34,15 @@ with mp_face_mesh.FaceMesh(
         face_count = 0
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                face_count += 1
+                face_count +=1
 
                 mp_drawing.draw_landmarks(
-                    image=frame,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec
-                )
-
-        frame = cv2.resize(frame, (0, 0), fx=0.8, fy=0.8)
-        stframe.image(frame, channels="BGR", use_column_width=True)
+                    image = frame,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec = drawing_spec,
+                    connection_drawing_spec = drawing_spec
+            )
+        
+        frame = cv2.resize(frame, (0,0), fx = 0.8, fy = 0.8)
+        stframe.image (frame, channels = "BGR", use_column_width = True)
